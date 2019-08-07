6b40e44f3a7b9a06cb6a8d06c0828246d636b018
Legion <rom4ik730@gmail.com>
2019-08-07 15:01:14 +0300

    Data binding added. MainActivity.kt changed in order to use binding.

--- src/main/java/com/legion1900/booksearch/MainActivity.kt
+++ src/main/java/com/legion1900/booksearch/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.legion1900.booksearch
 
 import android.content.Context
+import android.databinding.DataBindingUtil
 import android.net.Uri
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
@@ -11,25 +12,21 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
+import com.legion1900.booksearch.databinding.ActivityMainBinding
 import java.net.URL
 import kotlin.io.readText
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var buttonSearch: ImageButton
-    private lateinit var queryField: EditText
-    private lateinit var tvResult: TextView
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        buttonSearch = findViewById(R.id.button_search)
-        queryField = findViewById(R.id.et_query)
-        tvResult = findViewById(R.id.tv_query_result)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        buttonSearch.setOnClickListener {
+        binding.buttonSearch.setOnClickListener {
             val task = ApiCallTask()
-            task.execute(buildQuery(queryField.text.toString()))
+            task.execute(buildQuery(binding.etQuery.text.toString()))
         }
     }
 
@@ -49,10 +46,12 @@ class MainActivity : AppCompatActivity() {
     private inner class ApiCallTask() : AsyncTask<URL, Unit, String?>() {
         override fun onPreExecute() {
             super.onPreExecute()
-            buttonSearch.visibility = View.GONE
-            queryField.visibility = View.GONE
-            queryField.hideKeyboard()
-            tvResult.visibility = View.VISIBLE
+            binding.run {
+                buttonSearch.visibility = View.GONE
+                etQuery.visibility = View.GONE
+                etQuery.hideKeyboard()
+                tvQueryResult.visibility = View.VISIBLE
+            }
         }
 
         override fun doInBackground(vararg params: URL?): String? {
@@ -63,7 +62,7 @@ class MainActivity : AppCompatActivity() {
 
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
-            tvResult.text = result
+            binding.tvQueryResult.text = result
         }
     }
 
