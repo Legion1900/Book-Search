88a03a2a8f0807541511fb9d32a48ec4ebb6e100
Chumachenko Roman <rom4ik730@gmail.com>
2019-08-23 16:25:53 -0700

    Repo was moved to the root folder of project

---
+++ app/src/main/java/com/legion1900/bookserach/utilities/NetUtils.kt
@@ -0,0 +1,31 @@
+package com.legion1900.bookserach.utilities
+
+import android.net.Uri
+import com.legion1900.booksearch.BuildConfig
+import java.net.URL
+
+private val SCHEME = "https"
+private val AUTHORITY = "www.goodreads.com"
+private val PATH = "search.xml"
+private val PARAM_Q = "q"
+private val PARAM_KEY = "key"
+private val PARAM_PAGE = "p"
+
+
+fun buildQuery(
+    query: String,
+    scheme: String = SCHEME,
+    authority: String = AUTHORITY,
+    path: String = PATH,
+    page: Int = 0
+    ): URL {
+    val uri = Uri.Builder()
+        .scheme(scheme)
+        .authority(authority)
+        .appendPath(path)
+        .appendQueryParameter(PARAM_KEY, BuildConfig.apiKey)
+        .appendQueryParameter(PARAM_Q, query)
+        .appendQueryParameter(PARAM_PAGE, page.toString())
+        .build()
+    return URL(uri.toString())
+}
