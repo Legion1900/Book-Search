a924480fc96dfa005aaa480f9ca9379789e759bd
Chumachenko Roman <rom4ik730@gmail.com>
2019-08-25 19:31:19 -0700

    Implemented BookAdapter with BookViewHolder. Added layout for items.

---
+++ app/src/main/java/com/legion1900/bookserach/BookAdapter.kt
@@ -0,0 +1,38 @@
+package com.legion1900.bookserach
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.legion1900.booksearch.R
+import com.legion1900.booksearch.parser.Work
+
+class BookAdapter(context: Context, var dataSource: List<Work>) : RecyclerView.Adapter<BookAdapter.BookViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
+        val itemLayout = R.layout.book_item_view
+        val inflator = LayoutInflater.from(parent.context)
+        val view = inflator.inflate(itemLayout, parent, false)
+        return BookViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = dataSource.size
+
+    override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
+        val data = dataSource[position]
+        holder.apply {
+            author.text = data.author.name
+            title.text = data.title
+            avgRating.text = data.avgRating.toString()
+        }
+    }
+
+    class BookViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val author = itemView.findViewById<TextView>(R.id.author)
+        val title: TextView = itemView.findViewById<TextView>(R.id.title)
+        val avgRating: TextView = itemView.findViewById<TextView>(R.id.avg_rating)
+    }
+
+}
